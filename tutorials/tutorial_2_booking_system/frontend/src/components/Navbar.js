8b89e5b39e51c04e5b3454fef9da6507edfa0bd9
Alexcious91 <radiopanetiisetso@gmail.com>
2024-07-07 13:40:05 +0200

    Added token check

--- tutorials/tutorial_2_booking_system/frontend/src/components/Navbar.js
+++ tutorials/tutorial_2_booking_system/frontend/src/components/Navbar.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Nav, Container } from "react-bootstrap";
+import AuthContext from '../context/authContext';
 
 function NavbarComponent(props) {
+   const { token, userId, tokenExpiration } = useContext(AuthContext);
+
    return (
       <>
          <Navbar expand="lg" className='bg-secondary'>
             <Container>
                <Navbar.Brand href="#">EasyEvent</Navbar.Brand>
                <Navbar.Toggle aria-controls="navigation" />
-               
+
                <Navbar.Collapse id="navigation">
                   <Nav className="ml-auto">
-                     <Nav.Item>
-                        <Nav.Link href='/auth'>Authenticate</Nav.Link>
-                     </Nav.Item>
+                     {!token && // check if token doesnt exist
+                        <Nav.Item>
+                           <Nav.Link href='/auth'>Authenticate</Nav.Link>
+                        </Nav.Item>
+                     }
 
                      <Nav.Item>
                         <Nav.Link href='/events'>Events</Nav.Link>
                      </Nav.Item>
-                     
-                     <Nav.Item>
-                        <Nav.Link href='/bookings'>Bookings</Nav.Link>
-                     </Nav.Item>
+
+                     {token && // check if token does exist
+                        <Nav.Item>
+                           <Nav.Link href='/bookings'>Bookings</Nav.Link>
+                        </Nav.Item>
+                     }
                   </Nav>
                </Navbar.Collapse>
             </Container>
